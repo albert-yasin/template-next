634b2d46b6ddfbb4108f3712cf009211c724dd68
Albert <albert.yasin@japfa.com>
2025-06-03 08:42:08 +0700

    initial commit
    A Next project template to get started

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <main>
+      <h1> Welcome to my About Page Hehe</h1>
+    </main>
+  );
+}
---
+++ src/app/api/linkedin/route.ts
@@ -0,0 +1,7 @@
+export async function GET() {
+  return Response.json({ message: 'Hello from LinkedIn API' });
+}
+
+export async function POST() {
+  return Response.json({ message: 'Hello from LinkedIn API Post' });
+}
